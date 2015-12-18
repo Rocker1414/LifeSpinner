988c36c338658fd6ee78c9183e063fb671f4b5be
Chris <cdrogers@iastate.edu>
2015-12-17 18:46:18 -0600

    Added RandSpin and Spinner

---
+++ src/RandSpin.java
@@ -0,0 +1,10 @@
+// Michael Gargano
+// Chris Rogers
+// Game of Life Spinner
+// Returns int number with corresponding string color 
+public class RandSpin {
+	public RandSpin(String color, int num){
+		color = getColor();
+		num = getNum();
+	}
+}
