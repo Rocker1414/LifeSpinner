8bce47564ef6b16c42b3f6eabf8159e25633cc80
Chris <cdrogers@iastate.edu>
2015-12-17 18:18:00 -0600

    Added Eclipse src folder

---
+++ src/Spinner.java
@@ -0,0 +1,11 @@
+// Michael Gargano
+// Chris Rogers
+// Game of Life Spinner
+// Spins to a random color/number and outputs the results
+// in a GUI
+
+public class Spinner {
+	public static void main(String[] args){
+		System.out.println("test");
+	}
+}
